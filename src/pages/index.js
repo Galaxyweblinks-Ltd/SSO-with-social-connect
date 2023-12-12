4b908f212b2be50f1d5b69093709afc79299e40b
lok1984 <148316431+lok1984@users.noreply.github.com>
2023-12-12 12:13:44 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,20 @@
+// Importing the LoginWrapper component from the 'modules' directory
 import LoginWrapper from 'modules/LoginWrapper';
+
+// Importing the React library
 import React from 'react';
 
+// Functional component named Index
 function Index() {
+  // Returning the JSX for the Index component
   return (
+    // Using React.Fragment to group multiple components without adding extra nodes to the DOM
     <React.Fragment>
+      {/* Rendering the LoginWrapper component */}
       <LoginWrapper />
     </React.Fragment>
   );
 }
+
+// Exporting the Index component as the default export
 export default Index;
